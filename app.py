71c88b8a7b69d4356ea856bd5ec896668fa07f6a
Felix Alejandro Guzman <felixalejandro@outlook.com>
2020-03-31 18:12:30 -0400

    Agregando diccionario en espanol y API para dic

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import json
+
 from flask import Flask
+
+from text import sugerir_palabras, revisar_ortografia
 
 app = Flask(__name__)
 
@@ -6,6 +10,19 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 	return 'Hello World!'
+
+
+@app.route('/dic/<palabra>')
+def validar(palabra):
+	ortografia = revisar_ortografia(palabra)
+
+	data = {'orto': ortografia}
+
+	if not ortografia:
+		data['sugerencias'] = sugerir_palabras(palabra)
+
+	json_data = json.dumps(data)
+	return json_data
 
 
 if __name__ == '__main__':
---
+++ text.py
@@ -0,0 +1,28 @@
+import os
+
+import hunspell
+
+# hobj = hunspell.HunSpell('/home/felixal/PycharmProjects/legabot/legabot/archivos/diccionarios/es_ANY.dic',
+#  '/home/felixal/PycharmProjects/legabot/legabot/archivos/diccionarios/es_ANY.aff')
+from settings import APP_STATIC
+
+es_dic = os.path.join(APP_STATIC, 'es_ANY.dic')
+es_aff = os.path.join(APP_STATIC, 'es_ANY.aff')
+
+dic = hunspell.HunSpell(es_dic, es_aff)
+
+
+def revisar_ortografia(palabra: str) -> bool:
+	"""
+	Funcion para revisar una palabra ortograficamente
+	:param: palabra Palabra a revisar
+	:return: True si esta bien la palabra
+	"""
+	return dic.spell(palabra)
+
+
+def sugerir_palabras(palabra: str):
+	"""
+	Funcion para sugerir palabras
+	"""
+	return dic.suggest(palabra)
