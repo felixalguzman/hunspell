7b155c576f90d013ded9ff5f6ebb3d848a12af91
Felix Alejandro Guzman <felixalejandro@outlook.com>
2020-04-01 01:56:38 +0000

    Agregando plantilla

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask
+from flask import Flask, render_template
 
 from text import sugerir_palabras, revisar_ortografia
 
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello World!'
+	return render_template('index.html')
 
 
 @app.route('/dic/<palabra>')
